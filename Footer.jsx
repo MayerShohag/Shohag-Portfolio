1130bf9708e2916417178692b635473145b653a9
SHOHAG KUMAR <118052454+MayerShohag@users.noreply.github.com>
2026-01-21 01:05:21 +0600

    get in touch section updated

---
+++ Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="text-center">
+     <hr className="border-t border-white/30"/>
+      <h1 className="xl:text-[150px] lg:text-[110px] md:text-[85px] text-[40px] text-white/30 font-semibold">
+        SHOHAG KUMAR
+      </h1>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ Works.jsx
@@ -0,0 +1,10 @@
+const Works = () => {
+  return (
+    <div>
+      I'm Works page
+      <h1>Shohag</h1>
+    </div>
+  );
+};
+
+export default Works;
