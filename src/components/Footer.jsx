61fffc41f92dec3e768fdc3f2560ac39aecbb8fb
SHOHAG KUMAR <118052454+MayerShohag@users.noreply.github.com>
2026-01-21 01:09:19 +0600

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 const Footer = () => {
-     return (
-          <footer className="text-center">
-               <h1 className="xl:text-[160px] lg:text-[110px] md:text-[85px] text-[40px] text-white/50 font-semibold">
-                    SHOHAG KUMAR
-               </h1>
-          </footer>
-     );
+  return (
+    <footer className="text-center">
+     <hr className="border-t border-white/30"/>
+      <h1 className="xl:text-[150px] lg:text-[110px] md:text-[85px] text-[40px] text-white/30 font-semibold">
+        SHOHAG KUMAR
+      </h1>
+    </footer>
+  );
 };
 
 export default Footer;
